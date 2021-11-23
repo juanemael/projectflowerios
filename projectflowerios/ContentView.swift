01c7747a8f193f77c00d3ba30bac87c3dd458b86
Juan Emmanuel <juan.dharmadjaya@student.sgu.ac.id>
2021-11-24 00:28:13 +0700

    Initial Commit

---
+++ projectflowerios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  projectflowerios
+//
+//  Created by Juan Emmanuel D. on 24/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ projectflowerios/projectfloweriosApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectfloweriosApp.swift
+//  projectflowerios
+//
+//  Created by Juan Emmanuel D. on 24/11/21.
+//
+
+import SwiftUI
+
+@main
+struct projectfloweriosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
