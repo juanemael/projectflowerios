03a33d5dc38230a9a58b70f4135b0ecc4004b60b
Juan Emmanuel <juan.dharmadjaya@student.sgu.ac.id>
2021-11-24 00:40:32 +0700

    test commit

--- projectflowerios/ContentView.swift
+++ projectflowerios/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world all!")
+        Text("Hello, world all this is a test!")
             .padding()
     }
 }
